18d25062e25d5e030aca5acbb64a1bad6250cb38
Alex M. Alavez <ower182.aw@gmail.com>
2024-07-03 23:37:39 -0600

    Creacion de conexion API

---
+++ src/Conexion.java
@@ -0,0 +1,27 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conexion {
+
+    public Moneda conectarApi(String moneda){
+
+        URI direccion =  URI.create("https://v6.exchangerate-api.com/v6/890203fdfaab3ea51cc0d7e6/latest/"+ moneda);
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri((direccion))
+                .build();
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(),Moneda.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No se encontro la pelicula.");
+        }
+    }
+
+}
---
+++ src/Moneda.java
@@ -0,0 +1,7 @@
+
+import java.util.Map;
+
+public record Moneda(String time_last_update_utc,
+                     String base_code,
+                     Map <String, Double> conversion_rates) {
+}
